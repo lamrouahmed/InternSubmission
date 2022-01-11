e9b3f73c82334a6557599bc146af19b19751026b
lamrouahmed <lamrouahmed@gmail.com>
2022-01-12 00:02:45 +0100

    filter functionality OK

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -8,9 +8,9 @@ import styles from "./Store.module.scss"
 function Store() {
   const LIMIT = 5
   const [products, setProducts] = useState([])
-  const [categories, setCategories] = useState([])  
+  const [category, setCategory] = useState('')  
   const [currentPage, setCurrentPage] = useState(1)
-  const [displayedProducts, setDisplayedProducts] = useState([])
+  const [sortBy, setSortBy] = useState('none')
 
   useEffect(() => {
     fetchProducts();
@@ -20,40 +20,47 @@ function Store() {
     const response = await fetch("https://fakestoreapi.com/products")
     const data = await response.json()
     setProducts(data)
-    const cats = [...new Set(data.map((product) => product.category))]
-    setCategories(cats);
-    handlePagination(currentPage, data)
+    
   }
   
   
-  const handlePagination = (selectedPage = 1, data = products) => { 
-    const start = selectedPage * LIMIT - LIMIT
-    const end = start + LIMIT
+  const handlePagination = (selectedPage = 1) => { 
     setCurrentPage(selectedPage);
-    setDisplayedProducts(data.slice(start, end))
   }
 
-  const sortProducts = (type) => {
-    setProducts(SORTS[type](products))
-    handlePagination(currentPage, products)
+  const handleSort = (type) => {
+    setSortBy(type)
   }
 
+   const handleFilter = (cat) => {
+    setCategory(cat)
+  }
 
   const SORTS = {
+    none: (products) => products,
     price: (products) => products.sort((a, b) => b.price - a.price),
     rating: (products) => products.sort((a, b) => b.rating.rate - a.rating.rate),
   }
 
+  const categories = [...new Set(products.map((product) => product.category))];
+
+  
+  const sortedProducts = SORTS[sortBy](products);
+  const filteredProducts = category ? sortedProducts.filter(product => product.category === category) : sortedProducts;
+
+  const start = currentPage * LIMIT - LIMIT;
+  const end = start + LIMIT;
+  const currentProducts = filteredProducts.slice(start, end);
+
   return (
     <>
-      
       <div className={styles.products}>
-        {displayedProducts.map((product) => (
+        {currentProducts.map((product) => (
           <Product {...product} key={product.id} />
         ))}
       </div>
       <div className={styles.paginationContainer}>
-        {Array(Math.ceil(products.length / 5))
+        {Array(Math.ceil(filteredProducts.length / 5))
           .fill()
           .map((e, i) => (
             <Pagination
@@ -64,11 +71,11 @@ function Store() {
             />
           ))}
       </div>
-      {/* <div className="sort">
-        <p onClick={() => sortProducts("price")}>price</p>
-        <p onClick={() => sortProducts("rating")}>rating</p>
-      </div> */}
-
+      <div className="sort">
+        <p onClick={() => handleSort("price")}>price</p>
+        <p onClick={() => handleSort("rating")}>rating</p>
+      </div>
+      {categories.map((cat, i) => <div onClick={() => handleFilter(cat)} key={i}>{cat}</div>)}      
     </>
   );
 }
